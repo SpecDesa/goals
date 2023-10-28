4bb30273fccc0ae991c1f81c78ce2cfd8fa3daf9
Daniel Eystberg Sorensen Anderson <daniel.eystberg.sorensen.anderson-cic@ibm.com>
2023-10-28 14:45:26 +0200

    Currently splitting working list app.js

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from "react-native";
+import { useState } from "react";
+import _ from 'lodash'
 
 export default function App() {
+  const [enteredGoalText, setEnterGoalText] = useState(''); 
+  const [goals, setGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnterGoalText(enteredText);
+  }
+  const addGoalHandler = () => {
+    if(enteredGoalText !== ''){
+
+      setGoals(currentCourseGoals => 
+        [...currentCourseGoals, 
+          {text: _.capitalize(enteredGoalText.trim()), key: Math.random().toString()}]
+          );
+
+      setEnterGoalText('');
+    }
+  }
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // Default take only space it needs, so fix with flex: 1, and put flex on childrens.
+    <View style={styles.appContainer}> 
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Input course goals" onChangeText={goalInputHandler} value={enteredGoalText}/>
+        <Button title="Add goal" onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalsContainer}>
+        {/* <ScrollView> // Great for limited amount of data like article
+          {goals.map(goal => <View style={styles.goalItem} key={goal}><Text style={styles.goalText}>{goal}</Text></View>)}
+        </ScrollView> */}
+        <FlatList // Better scroll. Its lazy loading.
+          data={goals}
+          renderItem={(itemData) => {
+            itemData.index;
+              return <View style={styles.goalItem}><Text style={styles.goalText}>{itemData.item.text}</Text></View>
+              }
+            }
+            // keyExtractor={item => item.id} // If no real id, can set this. Automatically 
+
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer:{
+    flex: 1, // takes 1/6 
+    padding: 50,
+    backgroundColor: "white",
+    paddingHorizontal: 16
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row", // Default columnm, and flex is enabled by default..
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc"
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5, // 5/6 size.
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    color: 'white',
+  }, 
+  goalText: {
+    color: "white"
+  }
 });
---
+++ components/GoalItem.tsx
@@ -0,0 +1,28 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ListRenderItemInfo,
+} from "react-native";
+import _ from "lodash";
+export const GoalItem = (itemData: ListRenderItemInfo<any>) => {
+  return (
+    <View style={styles.goalItem}>
+      <Text style={styles.goalText}>{itemData.item.text}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
+  },
+  goalText: {
+    color: "white",
+  },
+});
